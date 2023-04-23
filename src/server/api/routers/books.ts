0acd14069ef7aee2d99932e7d13f7acfbaa314cc
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-04-23 19:50:36 +0200

    feat: making order - server side

--- src/server/api/routers/books.ts
+++ src/server/api/routers/books.ts
@@ -1,25 +1,6 @@
 import { z } from 'zod'
-import { adminProcedure, createTRPCRouter, publicProcedure } from '../trpc'
+import { createTRPCRouter, publicProcedure } from '../trpc'
 import { bookBase } from '~/schemes/base/bookBase.scheme'
-
-export const addBookSchema = z.object({
-    title: bookBase.title,
-    coverImageUrl: bookBase.coverImageUrl,
-    price: bookBase.price,
-    pages: bookBase.pages,
-    stock: bookBase.stock,
-    description: bookBase.description,
-    publishedAt: bookBase.publishedAt,
-    publisherId: bookBase.publisherId,
-    authorsIds: bookBase.authorsIds,
-    categoriesIds: bookBase.categoriesIds,
-    format: bookBase.format,
-    coverType: bookBase.coverType,
-})
-
-export const addBooksSchema = z.object({
-    books: addBookSchema.array(),
-})
 
 export const getBookByIdSchema = z.object({
     id: bookBase.id,
@@ -30,39 +11,11 @@ export const getBestSellersSchema = z.object({
     take: z.number().int().positive(),
 })
 
-export const bookRouter = createTRPCRouter({
-    addBooks: adminProcedure
-        .input(addBooksSchema)
-        .mutation(({ ctx, input }) => {
-            const { books } = input
+export const getRecentlyAddedSchema = z.object({
+    take: z.number().int().positive(),
+})
 
-            return ctx.prisma.book.createMany({
-                data: books.map((book) => {
-                    return {
-                        ...book,
-                        authors: {
-                            connect: book.authorsIds.map((a) => ({ id: a })),
-                        },
-                        categories: {
-                            connect: book.categoriesIds.map((c) => ({ id: c })),
-                        },
-                    }
-                }),
-            })
-        }),
-    addBook: publicProcedure.input(addBookSchema).mutation(({ ctx, input }) => {
-        return ctx.prisma.book.create({
-            data: {
-                ...input,
-                authors: {
-                    connect: input.authorsIds.map((a) => ({ id: a })),
-                },
-                categories: {
-                    connect: input.categoriesIds.map((c) => ({ id: c })),
-                },
-            },
-        })
-    }),
+export const bookRouter = createTRPCRouter({
     getOneById: publicProcedure
         .input(getBookByIdSchema)
         .query(({ ctx, input }) => {
@@ -79,25 +32,37 @@ export const bookRouter = createTRPCRouter({
         .query(({ ctx, input }) => {
             const { take, lastDays } = input
 
-            const since = new Date(
-                Date.now() * lastDays * 60 * 60 * 1000,
-            ).toISOString()
+            const since = new Date(Date.now() - lastDays * 60 * 60 * 1000)
 
             return ctx.prisma.book.findMany({
+                take,
                 where: {
-                    order: {
+                    orderItems: {
                         every: {
-                            createdAt: {
-                                gte: since,
+                            order: {
+                                createdAt: {
+                                    gte: since,
+                                },
                             },
                         },
                     },
                 },
-                take,
                 orderBy: {
-                    order: {
+                    orderItems: {
                         _count: 'desc',
                     },
+                },
+            })
+        }),
+    getRecentlyAdded: publicProcedure
+        .input(getRecentlyAddedSchema)
+        .query(({ ctx, input }) => {
+            const { take } = input
+
+            return ctx.prisma.book.findMany({
+                take,
+                orderBy: {
+                    createdAt: 'desc',
                 },
             })
         }),
